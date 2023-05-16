0c95235695b69806e2ad3cbb58764f707328fe35
bobur2006coder <boburdilmurodov702@gmail.com>
2023-05-16 19:17:22 +0500

    fix

---
+++ javascript.js
@@ -0,0 +1,40 @@
+let heart = document.querySelector("#in-hard");
+let btnAdd = document.querySelector(".plus");
+let btnMinus = document.querySelector(".minus");
+
+
+let add =()=>{
+    let price = document.querySelector(".value");
+    sum = Number(price.innerHTML);
+    sum +=10;
+    price.innerHTML=Number(sum)
+}
+
+btnAdd.addEventListener("click",add);
+
+
+let remove =()=>{
+    let price = document.querySelector(".value");
+    sum = Number(price.innerHTML);
+    sum -=10;
+    if (sum <= -0) {
+        return;
+    }
+    price.innerHTML=Number(sum);
+}
+
+btnMinus.addEventListener("click",remove)
+
+let evenRed =()=>{
+    
+    if (heart.style.color="black") {
+        heart.style.color="red";
+        return;
+    }
+}
+
+heart.addEventListener("click",evenRed)
+
+
+
+
